1b1288fb68e226f1a0b75242dcef0ffa84a92a6a
Feliphe Yuri <152447586+FelipheYuri@users.noreply.github.com>
2025-02-19 00:34:16 -0300

    mensagem.js

---
+++ mensagem.js
@@ -0,0 +1,103 @@
+let animationBox = document.querySelector(".text-section");
+
+let delay = setTimeout(() => {
+    setTimeout(() => {
+        animationBox.children[0].innerHTML = "|";
+
+        setTimeout(() => {
+            animationBox.children[0].innerHTML = "M|";
+
+            setTimeout(() => {
+                animationBox.children[0].innerHTML = "Me|";
+                
+                setTimeout(() => {
+                    animationBox.children[0].innerHTML = "Meu|";
+
+                    setTimeout(() => {
+                        animationBox.children[0].innerHTML = "Meu |";
+
+                        setTimeout(() => {
+                            animationBox.children[0].innerHTML = "Meu n|";
+
+                            setTimeout(() => {
+                                animationBox.children[0].innerHTML = "Meu no|";
+
+                                setTimeout(() => {
+                                    animationBox.children[0].innerHTML = "Meu nom|";
+
+                                    setTimeout(() => {
+                                        animationBox.children[0].innerHTML = "Meu nome|";
+
+                                        setTimeout(() => {
+                                            animationBox.children[0].innerHTML = "Meu nome |";
+        
+                                            setTimeout(() => {
+                                                animationBox.children[0].innerHTML = "Meu nome é|";
+        
+                                                setTimeout(() => {
+                                                    animationBox.children[0].innerHTML = "Meu nome é |";
+        
+                                                    setTimeout(() => {
+                                                        animationBox.children[0].innerHTML = "Meu nome é F|";
+        
+                                                        setTimeout(() => {
+                                                            animationBox.children[0].innerHTML = "Meu nome é Fe|";
+        
+                                                            setTimeout(() => {
+                                                                animationBox.children[0].innerHTML = "Meu nome é Fel|";
+        
+                                                                setTimeout(() => {
+                                                                    animationBox.children[0].innerHTML = "Meu nome é Felp|";
+        
+                                                                    setTimeout(() => {
+                                                                        animationBox.children[0].innerHTML = "Meu nome é Felps|";
+                                                                        
+                                                                        setTimeout(() => {
+                                                                            animationBox.children[0].innerHTML = "Meu nome é Felp|";
+        
+                                                                            setTimeout(() => {
+                                                                                animationBox.children[0].innerHTML = "Meu nome é Fel|";
+        
+                                                                                setTimeout(() => {
+                                                                                    animationBox.children[0].innerHTML = "Meu nome é Feli|";
+        
+                                                                                    setTimeout(() => {
+                                                                                        animationBox.children[0].innerHTML = "Meu nome é Felip|";
+        
+                                                                                        setTimeout(() => {
+                                                                                            animationBox.children[0].innerHTML = "Meu nome é Feliph|";
+        
+                                                                                            setTimeout(() => {
+                                                                                                animationBox.children[0].innerHTML = "Meu nome é Feliphe|";
+        
+                                                                                                setInterval(() => {
+                                                                                                    animationBox.children[0].innerHTML = "Meu nome é Feliphe";
+                                                                                                }, 1500);
+        
+                                                                                                setInterval(() => {
+                                                                                                    animationBox.children[0].innerHTML = "Meu nome é Feliphe|";
+                                                                                                }, 3000);
+                                                                                            }, 100);
+                                                                                        }, 100);
+                                                                                    }, 100);
+                                                                                }, 100);
+                                                                            }, 100);
+                                                                        }, 100);
+                                                                    }, 100);
+                                                                }, 100);
+                                                            }, 100);
+                                                        }, 100);
+                                                    }, 100);
+                                                }, 100);
+                                            }, 100);
+                                        }, 100);
+                                    }, 100);
+                                    }, 100);
+                                }, 100);
+                        }, 100);
+                    }, 100);
+                }, 100);
+            }, 100);
+        }, 100);
+    }, 100);
+}, 1000);
